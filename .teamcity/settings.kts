4b5fd78beffe665e33308821f7d9324174cf7bc6
admin <admin@TeamCity>
2020-08-30 15:32:35 +0000

    TeamCity change in 'Kotlin' project: triggers of 'testbuild' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -46,6 +47,13 @@ object Testbuild : BuildType({
             goals = "clean test"
             pomLocation = ".teamcity/pom.xml"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+    }
+
+    triggers {
+        vcs {
+            triggerRules = "+:**"
+            branchFilter = ""
         }
     }
 })
