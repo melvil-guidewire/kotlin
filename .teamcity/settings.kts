93b8d14db7d8b72082963b8a1a42d5e434d5543c
admin <admin@TeamCity>
2020-09-06 16:13:53 +0000

    TeamCity change in 'Kotlin' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -44,7 +44,7 @@ object Build : BuildType({
     steps {
         script {
             name = "list files"
-            scriptContent = "find ."
+            scriptContent = "find ./*"
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
             dockerRunParameters = """
                 -v /var/run/docker.sock:/var/run/docker.sock
