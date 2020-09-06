b564dc12be5cac6422ab3524a44e719136397006
admin <admin@TeamCity>
2020-09-06 19:29:53 +0000

    TeamCity change in 'Kotlin' project: triggers of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -55,7 +55,7 @@ object Build : BuildType({
 
     triggers {
         vcs {
-            triggerRules = "+:user=melvilgit;root=${DslContext.settingsRoot.id};comment=how are you:**"
+            triggerRules = "+:user=melvilgit;root=${DslContext.settingsRoot.id}:**"
 
             branchFilter = ""
         }
